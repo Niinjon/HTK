597bacdf8474d6f23da652f3f55f88db22b4f801
Niinjon <87705387+Niinjon@users.noreply.github.com>
2024-12-19 13:14:05 +0100

    Add files via upload

---
+++ Scripts/Linux/WEB.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+    echo "Root jogokkal kell futtatni!"
+    exit 1
+fi
+
+echo "Apache2 webszerver telepítése"
+apt-get update && apt-get install apache2 -y
